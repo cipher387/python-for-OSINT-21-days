edea53796588c13bda203dd52dd41ed96041f0a2
Cyb_detective <66636466+cipher387@users.noreply.github.com>
2023-04-08 05:29:00 +0300

    Create create_pdf.py

---
+++ Day_17/create_pdf.py
@@ -0,0 +1,15 @@
+from fpdf import FPDF
+
+
+pdfFile = FPDF()
+
+pdfFile.add_page()
+ 
+pdfFile.set_font("Arial", size = 12)
+
+pdfFile.cell(20, 10, txt = "Report text",
+         ln = 2, align = 'C')
+
+pdfFile.image('histogram.png', 10, 40, 30)
+
+pdfFile.output("report.pdf") 
