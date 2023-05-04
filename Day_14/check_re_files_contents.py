4228d315d11e22617461f5bf9f522bbf90202ce0
Cyb_detective <66636466+cipher387@users.noreply.github.com>
2023-05-04 17:16:19 +0300

    Update check_re_files_contents.py

--- Day_14/check_re_files_contents.py
+++ Day_14/check_re_files_contents.py
@@ -1,12 +1,13 @@
 import glob, os, re 
 
+regexp = re.compile('[a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+')
+
 os.chdir("test_dir")
 
 for file in glob.glob("*"): 
 
     with open (file) as current_file:
-        current_file_content=current_file.read()
-        regexp = re.compile('[a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+')
+        current_file_content=current_file.read()       
         if regexp.search(current_file_content):
             print("Math in file: "+file)
             print(current_file_content)
