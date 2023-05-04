b4732ea072593572093177a4ec51be533abc2107
Cyb_detective <66636466+cipher387@users.noreply.github.com>
2023-05-04 19:06:59 +0300

    Update create_pdf.py

--- Day_17/create_pdf.py
+++ Day_17/create_pdf.py
@@ -7,8 +7,7 @@ pdfFile.add_page()
  
 pdfFile.set_font("Arial", size = 12)
 
-pdfFile.cell(20, 10, txt = "Report text",
-         ln = 2, align = 'C')
+pdfFile.cell(20, 10, txt = "Report text", ln = 2, align = 'C')
 
 pdfFile.image('histogram.png', 10, 40, 30)
 
