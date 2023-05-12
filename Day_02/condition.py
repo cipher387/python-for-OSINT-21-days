e0464a5377d42874f35b15d4fb4bc8fd1acf67a9
mzarchi <me@zartweety.ir>
2023-05-12 23:20:40 +0330

    Reformat code

--- Day_02/condition.py
+++ Day_02/condition.py
@@ -1,7 +1,7 @@
-age = input('How old are you?\n')
+age = input('How old are you? ')
 
 
-if int(age) > 27:
+if int(age) >= 27:
     print("You are so old")
 elif int(age) < 27:
     print("You are so young")
