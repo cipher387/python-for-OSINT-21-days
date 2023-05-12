8c776be7ff81fa7074fc3174a4143a66f21d8b0f
mzarchi <me@zartweety.ir>
2023-05-12 23:25:50 +0330

    Remove unused import and reformat code

--- Day_06/read_list_of_fields.py
+++ Day_06/read_list_of_fields.py
@@ -1,11 +1,10 @@
-import json
 import requests
 
 response = requests.get("https://api.github.com/search/users?q=javascript")
 
-json_data=response.json()
+json_data = response.json()
 
 usersCount = len(json_data['items'])-1
 
 for x in range(usersCount):
-    print (json_data['items'][x]['html_url'])
+    print(json_data['items'][x]['html_url'])
--- Day_06/read_one_field.py
+++ Day_06/read_one_field.py
@@ -1,10 +1,9 @@
-import json
 import requests
 
 response = requests.get("https://api.github.com/search/users?q=javascript")
 
-json_data=response.json()
+json_data = response.json()
 
-print (json_data['total_count'])
+print(json_data['total_count'])
 
-print (json_data['items'][0]['html_url'])
+print(json_data['items'][0]['html_url'])
